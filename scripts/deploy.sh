c0143f0094987bf58d9deecbd1d8deab48694dfa
Chafah Sani <125821852+CHAFAH@users.noreply.github.com>
2024-11-19 09:48:05 +0100

    Add files via upload

---
+++ scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PLATFORM=$1
+echo "Deploying $PLATFORM software"
+# Add deployment logic here
---
+++ scripts/smoke-test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PLATFORM=$1
+echo "Performing smoke tests for $PLATFORM"
+# Add smoke test logic here
